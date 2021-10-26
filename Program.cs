6e9bb99c941d9b538a7b7012a1af18dadb914fac
Darmin Poturovic <d.poturovic@gmail.com>
2021-10-26 15:14:49 +0200

    newUppdate3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DataBaseAssigment.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataBaseAssigment
 {
@@ -6,7 +10,46 @@ namespace DataBaseAssigment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Calls the method 
+            ListApartments();
+        }
+        static private void ListApartments()
+        {
+
+            //create a object of apartmentContext and call it db 
+            using (ApartmentContext db = new ApartmentContext())
+            {
+                //Create a list of apartments(in databese) which is called apartmentList
+
+
+
+                List<Apartment> apartmentList = db.Apartments
+                    .Include(fd => fd.apartmentApplicants)
+                    
+                    .ThenInclude(b => b.Applicant)
+                    
+                    .ToList();
+
+                //Create a foreach to loop trhough my list 
+                foreach (var apartment in apartmentList)
+                {
+
+                    //creates en ampty variabel
+                    string applicant = "";
+
+                    //Create a new foreach to loop trhough my conected table(apartmentAplicant)
+                    foreach (var apartmentApplicant in apartment.apartmentApplicants)
+                    {
+                        applicant += apartmentApplicant.Applicant.Name + " ";
+                    }
+
+                    //Prints database in the console
+                    Console.WriteLine(applicant + " "+ apartment.Area +" " + apartment.Rent + " " + apartment.Rooms);
+                }
+            }
         }
     }
 }
+
+
+            
