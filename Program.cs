5f011e16e09653ae0039029d490da8794967b828
Darmin Poturovic <d.poturovic@gmail.com>
2021-10-27 09:29:28 +0200

    Uppdate 21-10-27

--- Program.cs
+++ Program.cs
@@ -10,9 +10,37 @@ namespace DataBaseAssigment
     {
         static void Main(string[] args)
         {
-            //Calls the method 
+            //First prints out list of applicants in the console
+            Console.WriteLine("press a key to see list of applicants");
+            Console.ReadKey();
+            listApplicants();
+
+            //asks if you want to add new applicant
+            Console.WriteLine("press a key to add new applcant");
+            Console.ReadKey();
+
+            //asks to enter data and read it 
+            Console.WriteLine(" Enter the Applicant name ");
+            string aplicantName = Console.ReadLine();
+            Console.WriteLine(" Enter the Applicant age ");
+            int aplicantAge = Convert.ToInt32(Console.ReadLine());
+
+
+            //First calls method which create new applicant and than calls method to print udated list of applicants
+            CreateApplicant(aplicantName, aplicantAge);
+            Console.WriteLine("new applicant added to list! ");
+            listApplicants();
+
+
+            //Calls the method to list apartments and applicants 
+            Console.WriteLine("Press a key to see apartments and applicants ");
+            Console.ReadKey();
             ListApartments();
+           
+
         }
+
+        #region Read (CRUD SYSTEM)
         static private void ListApartments()
         {
 
@@ -48,8 +76,58 @@ namespace DataBaseAssigment
                 }
             }
         }
+        #endregion
+
+        #region Read List of applicants
+        //read applicants 
+        static private void listApplicants()
+        {
+            using (ApartmentContext db = new ApartmentContext())
+            {
+                List<Applicant> applicanList = db.Applicants.ToList();
+
+                foreach (var applicantN in applicanList)
+                {
+                    string newapplicant = applicantN.Name +  " " + applicantN.Age;
+
+                    Console.WriteLine(newapplicant);
+
+                }           
+            }
+        }
+        #endregion
+
+        #region Create new applicant (CRUD SYSTEM)
+        static void CreateApplicant(string applicantName, int age)
+        {
+            using (ApartmentContext db = new ApartmentContext())
+            {
+
+                Applicant newAplicant = new Applicant();
+
+                newAplicant.Name = applicantName;
+                newAplicant.Age = age;
+
+                db.Applicants.Add(newAplicant);
+                db.SaveChanges();
+            }
+        } 
+        #endregion
     }
+
 }
+             
+
+            
+
+               
+
+                    
+
+
+
+
+
 
 
             
